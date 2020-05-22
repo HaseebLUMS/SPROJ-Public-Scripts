3187475dc738d28a28e04646fab91f5bcd956514
Muhammad Haseeb <haseeb.lums@gmail.com>
2020-05-22 20:00:49 +0500

    Add files via upload

--- filter.py
+++ filter.py
@@ -1,39 +1,25 @@
-import glob
-import dpkt
-from dpkt.ip import IP
-import socket
 import json
-import sys
+with open("without_plugin_results_500.json") as f: without_plugin = json.load(f)
+with open("500_with_plugin2_results.json") as f: with_plugin = json.load(f)
+
+with_plugin_dict = {}
+with_plugin_url = []
+for ele in with_plugin:
+	with_plugin_dict[ele["url"]] = ele
+	with_plugin_url += [ele["url"]]
 
 
-with open("ip_range") as f: ips = set(f.read().split("\n"))
+without_plugin_dict = {}
+without_plugin_url = []
+for ele in without_plugin:
+	if ele["url"] not in with_plugin_url: continue
+	without_plugin_dict[ele["url"]] = ele
+	without_plugin_url += [ele["url"]]
 
+urls = without_plugin_url
+print(len(urls))
 
-data_folder = str(sys.argv[1])
-all_files = glob.glob("./"+data_folder+"/*.pcap")
-print("total pcaps: ", len(all_files))
+plt_without = [without_plugin_dict[url]["transferSize"] for url in urls]
+plt_with = [with_plugin_dict[url]["transferSize"] for url in urls]
 
-
-count_files = 0
-for f in all_files:
-	result = []
-	pcap = dpkt.pcap.Reader(open(f, 'rb'))
-	for ts, buf in pcap:
-		try:
-			eth = dpkt.ethernet.Ethernet(buf)
-			ip = eth.data
-			src = socket.inet_ntoa(ip.src)
-			tmp = (".".join((src.split(".")[:3])))+"."
-			if tmp not in ips: continue
-			dst = socket.inet_ntoa(ip.dst)
-			tmp = (".".join((dst.split(".")[:3])))+"."
-			if tmp in ips: continue
-			result += [(ts, src, dst)]
-		except:
-			pass
-	
-	count_files += 1
-	print(count_files, " pcaps done.")
-	#one pcap is processed
-	fn = "results/" + (f.replace("/", "_").replace(".", "_") + "_processed.json")
-	with open(fn, "w") as f: f.write(json.dumps(result, indent=4))
+print(plt_with)
